f3b55d0b3a1efbb67d5447d3e07b4c3bec132cfc
Cameron White <cameronwhite4121@gmail.com>
2024-10-12 21:21:51 -0700

    go away obj

--- MTGDeckBuilder/obj/Debug/net8.0/MTGDeckBuilder.AssemblyInfo.cs
+++ MTGDeckBuilder/obj/Debug/net8.0/MTGDeckBuilder.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MTGDeckBuilder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d6499430bd32d29c3f5c945c1dff62c03c3dc88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+428218b185a1d3cf0d719258a82028769b8a0c2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MTGDeckBuilder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MTGDeckBuilder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
