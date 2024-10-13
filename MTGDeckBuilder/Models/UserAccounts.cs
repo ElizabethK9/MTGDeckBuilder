72970e7289d2cf70032ae267b3f393f1fbe93862
Cameron White <cameronwhite4121@gmail.com>
2024-10-12 18:21:01 -0700

    Added UserAccounts class, also why is the .vs file showing as modified again ugh

---
+++ MTGDeckBuilder/Models/UserAccounts.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MTGDeckBuilder.Models
+{
+    public class UserAccounts
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 10, ErrorMessage = "Email must be 10 - 50 characters")]
+        string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Password must be 5 - 30 characters")]
+        string Password {  get; set; }
+    }
+}
