efd7071b7fd6db3bc936e4426208722bda174d8d
Cameron White <cameronwhite4121@gmail.com>
2024-10-16 16:13:31 -0700

    Can now pull an image url from the api's server and display it on the page

---
+++ MTGDeckBuilder/Controllers/CardController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using MtgApiManager.Lib.Service;
+using MTGDeckBuilder.Models;
+
+namespace MTGDeckBuilder.Controllers
+{
+    public class CardController : Controller
+    {
+        [HttpGet]
+        public ActionResult CardSearch()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CardSearch(GameCard card)
+        {
+            if(ModelState.IsValid)
+            {
+                TempData["Card"] = card.CardName;
+                return RedirectToAction("ViewCard");
+            }
+            return View(card);
+        }
+
+        public async Task<ActionResult> ViewCard()
+        {
+            // User entered string for card search
+            String cardSearch = TempData["Card"] as String;
+
+            // Initilize MTG framework
+            IMtgServiceProvider serviceProvider = new MtgServiceProvider();
+            ICardService service = serviceProvider.GetCardService();
+
+            // Pass in cardSearch string into the framework
+            var result = await service.Where(x => x.Name, cardSearch)
+                                      .AllAsync();
+
+            // Card search result values         
+            string cardImageLink = result.Value[0].ImageUrl.ToString();
+
+            // Pass image url as a cardName object
+            GameCard card = new GameCard(cardImageLink);
+
+            return View(card);
+        }
+    }
+}
--- MTGDeckBuilder/Models/GameCard.cs
+++ MTGDeckBuilder/Models/GameCard.cs
@@ -13,8 +13,7 @@ namespace MTGDeckBuilder.Models
         /// Multiverse id of a card. Multiverse id is a unique id for each card
         /// in Magic that refers to each cards printed order from each set.
         /// </summary>
-        [Key]
-        public int CardMID { get; set; }
+        public int? CardMID { get; set; }
 
         /// <summary>
         /// Fullname of the card
@@ -26,22 +25,20 @@ namespace MTGDeckBuilder.Models
         /// Card type represents what kind of card it is. Magic has many card types like
         /// creatures, instants, sorceries, enchantments and more.
         /// </summary>
-        [Required]
-        public string CardType { get; set; }
+        public string? CardType { get; set; }
 
         /// <summary>
         /// Several cards have a subtype. Creatures are the best example of this with creature
         /// being the main type, and humans or beasts being its subtype.
         /// </summary>
-        public string CardSubtype { get; set; }
+        public string? CardSubtype { get; set; }
 
         /// <summary>
         /// All cards have a converted mana cost (referred to as CMC a lot of times). The cost could include any combination
         /// of the five main colors, or colorless mana costs. There are even cards with mana cost 0, or cards
         /// that don't apppear to have a mana cost, like tokens, which have a hidden mana cost of 0.
         /// </summary>
-        [Required]
-        public string ManaCost { get; set; }
+        public string? ManaCost { get; set; }
 
         /// <summary>
         /// A card's set is what set the card is released in. There are an abundance of card reprints in later sets,
@@ -49,32 +46,34 @@ namespace MTGDeckBuilder.Models
         /// Unglued, and more. There are over 100 card sets in Magic as the series debuted in 1993 and has been going
         /// strong since.
         /// </summary>
-        [Required]
-        public string CardSet { get; set; }
+        public string? CardSet { get; set; }
 
         /// <summary>
         /// Will be implemented in the future when the site is more fleshed out.
         /// </summary>
-        public int CardPrice { get; set; }
+        public int? CardPrice { get; set; }
 
         /// <summary>
         /// Int value for the attacking power of the card if it's a creature. Can include 0.
         /// </summary>
-        public int CreaturePower { get; set; }
+        public int? CreaturePower { get; set; }
 
         /// <summary>
         /// Int value for the defensive power of the card if it's a creature. Can include 0.
         /// </summary>
-        public int CreatureToughness { get; set; }
+        public int? CreatureToughness { get; set; }
 
         /// <summary>
         /// Unlinke the mulitverse id, collector number represents the order the
         /// card was printed from the specific set
         /// </summary>
-        public int CollectorNumber { get; set; }
+        public int? CollectorNumber { get; set; }
+
+        // Parameterless constructor
+        public GameCard() { }
 
         /// <summary>
-        /// Constructor for the game card (not including price)
+        /// Full constructor for the game card
         /// </summary>
         /// <param name="cardMID"></param>
         /// <param name="cardName"></param>
@@ -82,12 +81,14 @@ namespace MTGDeckBuilder.Models
         /// <param name="cardSubtype"></param>
         /// <param name="manaCost"></param>
         /// <param name="cardSet"></param>
+        /// <param name="cardPrice"></param>
         /// <param name="creaturePower"></param>
         /// <param name="creatureToughness"></param>
         /// <param name="collectorNumber"></param>
         public GameCard(int cardMID, string cardName, string cardType,
                         string cardSubtype, string manaCost, string cardSet,
-                        int creaturePower, int creatureToughness, int collectorNumber)
+                        int cardPrice, int creaturePower, int creatureToughness, 
+                        int collectorNumber)
         {
             this.CardMID = cardMID;
             this.CardName = cardName;
@@ -95,9 +96,15 @@ namespace MTGDeckBuilder.Models
             this.CardSubtype = cardSubtype;
             this.ManaCost = manaCost;
             this.CardSet = cardSet;
+            this.CardPrice = cardPrice;
             this.CreaturePower = creaturePower;
             this.CreatureToughness = creatureToughness;
             this.CollectorNumber = collectorNumber;
+        }
+
+        public GameCard(string cardName) 
+        {
+            this.CardName=cardName;
         }
     }
 }
