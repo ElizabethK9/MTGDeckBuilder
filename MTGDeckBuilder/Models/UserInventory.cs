2ffc46e778c77bc6a08e7c89fc72ccb9bd2c31c4
Cameron White <cameronwhite4121@gmail.com>
2024-11-23 13:12:11 -0800

    Removed unused methods

--- MTGDeckBuilder/Models/UserInventory.cs
+++ MTGDeckBuilder/Models/UserInventory.cs
@@ -12,12 +12,16 @@ namespace MTGDeckBuilder.Models
     public class UserInventory
     {
         [Key] public int Id { get; set; }
+
         [ForeignKey("IdentityUser")] 
         public string IdentityUserId { get; set; }
+
         public List<GameDeck> AllDecks { get; set; }
         public List<GameCard> AllCards { get; set; }
+
         // User that this inventory belongs to
         public IdentityUser User{ get; set; } 
+
         public UserInventory()
         {
             AllDecks = new List<GameDeck>();
@@ -52,81 +56,6 @@ namespace MTGDeckBuilder.Models
             }
             context.GameDecks.Remove(deck);
             context.SaveChanges();
-        }
-
-        /// <summary>
-        /// Adds a card to the current user's card collection
-        /// </summary>
-        /// <param name="card"></param>
-        public void AddCard(ApplicationDbContext context, GameCard card)
-        {
-            // Dereference the inventory from the card
-            card.Inventory = this;
-
-            context.GameCards.Add(card);
-            context.SaveChanges();
-        }
-
-        /// <summary>
-        /// Removes a card from the current user's card collection
-        /// </summary>
-        /// <param name="card"></param>
-        public void RemoveCard(ApplicationDbContext context, GameCard card)
-        {
-            // Dereference the inventory from the card
-            card.Inventory = null;
-
-            context.GameCards.Remove(card);
-            context.SaveChanges();
-        }
-
-        /// <summary>
-        /// Adds a card to a specific deck in the current user's collection
-        /// </summary>
-        /// <param name="deck"></param>
-        /// <param name="card"></param>
-        public void AddCardToDeck(ApplicationDbContext context, GameDeck deck, GameCard card)
-        {
-            // Associate the deck with this inventory
-            deck.Inventory = this;
-
-            // Create a new DeckCard if it doesnt already exist
-            DeckCard deckCard = new DeckCard
-            {
-                GameDeckId = deck.Id,
-                GameCardMID = card.MID,
-                Quantity = 1
-            };
-
-            context.DeckCards.Add(deckCard);
-            context.SaveChanges();
-        }
-
-        /// <summary>
-        /// Removes a card from a specific deck in the current user's collection
-        /// </summary>
-        /// <param name="deck"></param>
-        /// <param name="card"></param>
-        public void RemoveCardFromDeck(ApplicationDbContext context, GameDeck deck, GameCard card)
-        {
-            // Find the deckcard where the MID's and Deck Id's are equal
-            DeckCard deckCard = (from dc in context.DeckCards
-                                 where dc.GameDeckId == deck.Id && dc.GameCardMID == card.MID
-                                 select dc).FirstOrDefault();
-
-            if (deckCard != null)
-            {
-                context.DeckCards.Remove(deckCard);
-                context.SaveChanges();
-            }
-        }
-
-        /// <summary>
-        /// Saves changes made to the user's inventory in the database.
-        /// </summary>
-        public async Task SaveChanges(ApplicationDbContext context)
-        {
-           await context.SaveChangesAsync();
         }
     }
 }
