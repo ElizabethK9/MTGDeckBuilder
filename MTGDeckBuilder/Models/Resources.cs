5d16bf0567e998ed83bc0cbc461d4d401ac3ac36
ElizabethK9 <167283322+ElizabethK9@users.noreply.github.com>
2024-11-25 14:56:13 -0800

    Updated the second Contact Information

--- MTGDeckBuilder/Models/Resources.cs
+++ MTGDeckBuilder/Models/Resources.cs
@@ -7,8 +7,8 @@
         public static string EmailOne { get; } = "Elizabethproctor66@gmail.com";
         public static string DiscordIDOne { get; } = "elizabeth_k9 - Message Only!";
 
-        public static string NameTwo { get; } = "";
-        public static string EmailTwo { get; } = "john.doe@example.com";
+        public static string NameTwo { get; } = "Cameron White";
+        public static string EmailTwo { get; } = "cameronwhite4121@gmail.com";
         public static string DiscordIDTwo { get; } = "cameronw.beans";
 
         // Static method to display contact information
